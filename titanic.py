f62528e07cf64d93cc36936b880d68b829ee9fa1
unknown <Jacobrdilonardo@gmail.com>
2023-10-31 17:07:39 +1100

    title feature added

--- titanic.py
+++ titanic.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 import xgboost as xgb
 import lightgbm as lgb
 
@@ -15,17 +15,18 @@ import lightgbm as lgb
 def main():
     df = pd.read_csv('train.csv')
     test_df = pd.read_csv("test.csv")
-    
+    print(df.tail())
+    print(test_df.tail())
     # ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
     # ['num_family_onboard', 'age_category_code', 'is_alone']
-    features = ['Pclass', 'sex_code', 'age_category_code', 'is_alone']
+    features = ['Pclass', 'Sex_code', 'age_category_code', 'is_alone', 'title_code']
 
     df = feature_engineer(df)
-    df = encode_data(df)
-
     test_df = feature_engineer(test_df)
-    test_df = encode_data(test_df)
+    df, test_df = encode_data(df, test_df, None)
 
+    print(df.tail())
+    print(test_df.tail())
     X = df[features]
     y = df["Survived"]
 
@@ -38,9 +39,15 @@ def main():
     for m in models:
         model = m()
         model.fit(X, y)
+        # model.fit(X_train, y_train)
         pred = model.predict(X_test)
 
-        # print(f"{m.__name__}: {accuracy_score(y_test, pred)}")
+        # print(f"{m.__name__}: Acc: {accuracy_score(y_test, pred)}")
+        # print(f"{m.__name__}: Prec: {precision_score(y_test, pred)}")
+        # print(f"{m.__name__}: Recall: {recall_score(y_test, pred)}")
+        # print(f"{m.__name__}: F1: {f1_score(y_test, pred)}")
+        # print("-"*100)
+        # print(f"{m.__name__}: Con: {confusion_matrix(y_test, pred)}")
 
         # Create a DataFrame with PassengerId and the corresponding predictions
         results = pd.DataFrame({'PassengerId': test_df['PassengerId'], 'Survived': np.round(pred).astype(int)})
@@ -49,23 +56,21 @@ def main():
         results.to_csv(f'{m.__name__}_predictions.csv', index=False)
 
 
-def encode_data(df):
-
+def encode_data(df, test_df, features):
+    features = ["Sex", 'Embarked', 'age_category', 'title']
     encoder = LabelEncoder()
-    df['name_code'] = encoder.fit_transform(df['Name'])
-    df['sex_code'] = encoder.fit_transform(df['Sex'])
-    df['ticket_code'] = encoder.fit_transform(df['Ticket'])
-    df['cabin_code'] = encoder.fit_transform(df['Cabin'])
-    df['embarked_code'] = encoder.fit_transform(df['Embarked'])
-    df['age_category_code'] = encoder.fit_transform(df['age_category'])
+    for feature in features:
+        encoder.fit(pd.concat([df[feature], test_df[feature]], axis=0))
+        df[f'{feature}_code'] = encoder.transform(df[feature])
+        test_df[f'{feature}_code'] = encoder.transform(test_df[feature])
 
-    return df
+    return df, test_df
 
 
 def feature_engineer(df):
     df['num_family_onboard'] = df['SibSp'] + df['Parch']
     df['Age'].fillna(df['Age'].median(), inplace=True)
-    df['title'] = ... # extarct title from name
+    df['title'] = df['Name'].apply(lambda x: x.split(",")[1].split(".")[0].strip())
     df['is_alone'] = df['num_family_onboard'].apply(lambda x: int(x > 0))
     df['age_category'] = df['Age'].map(map_age_to_weight_category)
 
@@ -73,12 +78,11 @@ def feature_engineer(df):
 
 
 def map_age_to_weight_category(age):
-
     if age < 19:
         return 'child'
-    elif 19 <= age < 25:
+    elif 19 <= age < 30:
         return 'young_adult'
-    elif 25 <= age < 55:
+    elif 30 <= age < 60:
         return 'adult'
     else:
         return 'senior'
